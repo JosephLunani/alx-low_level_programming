b9f2b1497391293f3c8f96fa6a44e2380a87a962
JosephLunani <jose1234lunani@gmail.com>
2022-12-13 22:29:03 -0800

    timetables for 0-15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints tables
+ */
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n > 15 || n < 0)
+	{
+		exit(0);
+	}
+	else
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+
+			for (mult = 1 ; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
