5bec7f4ced7aecf3b84050be78b796666da56455
JosephLunani <jose1234lunani@gmail.com>
2022-12-14 04:26:42 -0800

    even fibonacci total

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the even fibonacci less than 4,000, 000
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int fnum = 1, snum = 2;
+	int thirdnum;
+	int total;
+
+	for (thirdnum = 3; thirdnum < 4000000; thirdnum++)
+	{
+		thirdnum = fnum + snum;
+		fnum = snum;
+		snum = thirdnum;
+		if
+			(thirdnum / 2 == 0)
+				total = total + thirdnum;
+				printf("%d\n", total);
+	}
+}
