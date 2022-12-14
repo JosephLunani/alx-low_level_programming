3ea29f5350225ac0e7b5dea4d5a502731b2f17a4
JosephLunani <jose1234lunani@gmail.com>
2022-12-13 23:04:05 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,11 +14,10 @@ void print_times_table(int n)
 	}
 	else
 	{
-		for (num = 1; num <= n; num++)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar('0');
 
-			for (mult = 1 ; mult <= n; mult++)
+			for (mult = 0 ; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
