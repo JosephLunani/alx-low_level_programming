a893be0fd30ffdbbb0b5f350deca3fe881bc88dc
JosephLunani <jose1234lunani@gmail.com>
2022-12-15 04:15:33 -0800

    multiplies 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "mai.h"
+/**
+ * mul - multiply 2 integer
+ * @a: first integer
+ * @b: second integer
+ * Return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
